34de0bf5fa8f144d9bee794c60e28814c2bef0f9
Dherisma Hanindita <dherismadedek@gmail.com>
2022-03-15 20:08:31 +0700

    js4

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Praktikum3;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class NewsController extends Controller
+{
+    public function news($id){
+        return view('news');
+    }
+}
+
+?>
+
+?>
